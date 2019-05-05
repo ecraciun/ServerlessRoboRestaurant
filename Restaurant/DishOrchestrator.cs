a649ea5a68f35fef6d34c40ea0507bc9d205910b
Emil Craciun <emil.craciun@live.com>
2019-05-05 12:13:21 +0300

    Refactored DishOrchestrator. Implemented OrderOrchestrator.

--- Restaurant/DishOrchestrator.cs
+++ Restaurant/DishOrchestrator.cs
@@ -11,19 +11,16 @@ namespace Restaurant
         public static async Task<bool> RunOrchestrator(
             [OrchestrationTrigger] DurableOrchestrationContext context)
         {
-            var dishId = context.GetInput<string>();
-            if (!string.IsNullOrEmpty(dishId))
+            var dish = context.GetInput<Dish>();
+            if (dish != null && dish.Recipe != null && dish.Recipe.Steps != null)
             {
-                var dish = await context.CallActivityAsync<Dish>(Constants.GetDishActivityFunctionName, dishId);
-                if (dish != null && dish.Recipe != null && dish.Recipe.Steps != null)
+                foreach (var step in dish.Recipe.Steps)
                 {
-                    foreach (var step in dish.Recipe.Steps)
-                    {
-                        await context.CallActivityAsync(Constants.RecipeStepActivityFunctionName, step);
-                    }
-                    return true;
+                    await context.CallActivityAsync(Constants.RecipeStepActivityFunctionName, step);
                 }
+                return true;
             }
+
             return false;
         }
     }
--- Restaurant/OrderOrchestrator.cs
+++ Restaurant/OrderOrchestrator.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Core;
@@ -18,8 +20,94 @@ namespace Restaurant
         {
             var order = context.GetInput<Order>();
 
-            // Replace "hello" with the name of your Durable Activity Function.
-            await context.CallActivityAsync<string>("OrderOrchestrator_Hello", "London");
+            if( order != null && 
+                (order.OrderItems?.Any() ?? false) &&
+                order.LastModifiedUtc == order.TimePlacedUtc &&
+                order.Status == OrderStatus.New)
+            {
+                bool orderUpdated = await UpdateOrder(context, order, OrderStatus.Preparing);
+
+                var dishesToPrepare = await GetDishesToPrepare(context, order);
+
+                var neededIngredients = GetAllNeededIngredientsForOrder(dishesToPrepare);
+
+                var ingredientsReserved = await context.CallSubOrchestratorAsync<bool>(
+                    Constants.StockCheckerOrchestratorFunctionName, neededIngredients
+                    );
+
+                if (ingredientsReserved)
+                {
+                    await PrepareDishes(context, dishesToPrepare);
+
+                    orderUpdated = await UpdateOrder(context, order, OrderStatus.Ready);
+                }
+            }
+        }
+
+        private static async Task<bool> UpdateOrder(DurableOrchestrationContext context, Order order, OrderStatus stauts)
+        {
+            return await context.CallActivityAsync<bool>(
+                Constants.UpdateOrderActivityFunctionName,
+                new OrderDTO
+                {
+                    OrchestratorId = context.InstanceId,
+                    OrderId = order.Id,
+                    OrderStatus = stauts
+                });
+        }
+
+        private static async Task PrepareDishes(DurableOrchestrationContext context, List<Dish> dishesToPrepare)
+        {
+            var dishOrchestratorTasks = new List<Task<bool>>();
+            foreach (var dishToPrepare in dishesToPrepare)
+            {
+                dishOrchestratorTasks.Add(
+                    context.CallSubOrchestratorAsync<bool>(
+                        Constants.DishOrchestratorFunctionName, dishToPrepare));
+            }
+
+            var allPrepared = await Task.WhenAll(dishOrchestratorTasks);
+            // TODO: maybe check if all were done
+        }
+
+        private static async Task<List<Dish>> GetDishesToPrepare(DurableOrchestrationContext context, Order order)
+        {
+            var getDishesTasks = new List<Task<Dish>>();
+            foreach (var orderedItem in order.OrderItems)
+            {
+                getDishesTasks.Add(context.CallActivityAsync<Dish>(
+                    Constants.GetDishActivityFunctionName, orderedItem.DishId));
+            }
+
+            var dishes = await Task.WhenAll(getDishesTasks);
+            var dishesToPrepare = new List<Dish>();
+
+            foreach(var dish in dishes)
+            {
+                var quantity = order.OrderItems.FirstOrDefault(x => x.DishId == dish.Id).Quantity;
+                
+                while(quantity > 0)
+                {
+                    dishesToPrepare.Add(dish);
+                    quantity--;
+                }
+            }
+
+            return dishesToPrepare;
+        }
+
+        private static List<DishIngredient> GetAllNeededIngredientsForOrder(List<Dish> dishes)
+        {
+            var allIngredients = dishes.SelectMany(x => x.Ingredients)
+                .GroupBy(x => x.StockIngredientId)
+                .Select(x => new DishIngredient
+                {
+                    StockIngredientId = x.Key,
+                    Name = x.FirstOrDefault().Name,
+                    QuantityNeeded = x.Sum(s => s.QuantityNeeded)
+                })
+                .ToList();
+            return allIngredients;
         }
     }
 }
