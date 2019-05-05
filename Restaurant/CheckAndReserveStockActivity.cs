bc74018e98cafbc2775fca7e7a2de33de1189cb2
Emil Craciun <emil.craciun@live.com>
2019-05-05 11:35:45 +0300

    Finished the StockCheckerOrchestrator and CheckAndReserveStockActivity functions

--- Restaurant/CheckAndReserveStockActivity.cs
+++ Restaurant/CheckAndReserveStockActivity.cs
@@ -1,26 +1,51 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Core;
+using Core.Entities;
+using Core.Services.Interfaces;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Willezone.Azure.WebJobs.Extensions.DependencyInjection;
 
 namespace Restaurant
 {
     public static class CheckAndReserveStockActivity
     {
         [FunctionName(Constants.CheckAndReserveStockActivityFunctionName)]
-        public static async Task<bool> Run(
-            [ActivityTrigger]string orderId,
+        public static async Task<(bool Reserved, string IngredientName)> Run(
+            [ActivityTrigger]DishIngredient neededIngredient,
+            [Inject]IBaseRepositoryFactory<StockIngredient> stockRepositoryFactory,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            if (neededIngredient == null ||
+                string.IsNullOrEmpty(neededIngredient.StockIngredientId) ||
+                neededIngredient.QuantityNeeded <= 0) return (false, string.Empty);
 
-            return true;
+            var cosmosDbEndpoint = Environment.GetEnvironmentVariable(Constants.CosmosDbEndpointKeyName);
+            var cosmosDbKey = Environment.GetEnvironmentVariable(Constants.CosmosDbKeyKeyName);
+            var repo = stockRepositoryFactory.GetInstance(cosmosDbEndpoint, cosmosDbKey, Constants.StockCollectionName);
+
+            var stockIngredient = await repo.GetAsync(neededIngredient.StockIngredientId);
+            if (stockIngredient != null)
+            {
+                if (stockIngredient.StockQuantity - neededIngredient.QuantityNeeded > 0)
+                {
+                    var updated = await repo.TryUpdateWithRetry(stockIngredient, (si) =>
+                    {
+                        si.StockQuantity -= neededIngredient.QuantityNeeded;
+                    });
+
+                    if (updated)
+                    {
+                        return (true, string.Empty);
+                    }
+                }
+            }
+            else
+            {
+                //TODO: we have a problem, log maybe?
+            }
+            return (false, neededIngredient.Name);
         }
     }
 }
--- Restaurant/StockCheckerOrchestrator.cs
+++ Restaurant/StockCheckerOrchestrator.cs
@@ -1,13 +1,12 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Core;
 using Core.Entities;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Restaurant
 {
@@ -18,59 +17,88 @@ namespace Restaurant
             [OrchestrationTrigger] DurableOrchestrationContext context)
         {
             var neededIngredients = context.GetInput<List<DishIngredient>>();
-            var checkTasks = new List<Task<(bool Reserved, string IngredientName)>>();
 
-            foreach(var ingredient in neededIngredients)
+            var ingredientsToOrder = await HandleNeededStock(context, neededIngredients);
+
+            while (ingredientsToOrder.Any())
             {
-                checkTasks
-                    .Add(context.CallActivityAsync<(bool Reserved, string IngredientName)>(Constants.CheckAndReserveStockActivityFunctionName, ingredient));
-            }
+                var groupdResults = await GetNeededSuppliers(context, ingredientsToOrder);
 
-            var checkResult = await Task.WhenAll(checkTasks);
-            var ingredientsToOrder = checkResult.Where(x => x.Reserved == false).Select(x => x.IngredientName).ToList();
+                await CreateSupplierOrders(context, groupdResults);
 
-            if (ingredientsToOrder.Any())
-            {
-                var supplierQueryTasks = new List<Task<SupplierQueryResponse>>();
+                var leftOverNeededIngredients = neededIngredients
+                    .Where(x => ingredientsToOrder.Contains(x.Name)).ToList();
 
-                foreach(var ingredient in ingredientsToOrder)
-                {
-                    supplierQueryTasks.Add(context.CallActivityAsync<SupplierQueryResponse>(Constants.SupplierFinderActivityFunctionName,
-                        new SupplierQueryRequest
-                        {
-                            IngredientName = ingredient,
-                            QueryStrategy = SupplierQueryStrategy.OptimizeDelivery
-                        }));
-                }
-
-                var supplierQueryResponses = await Task.WhenAll(supplierQueryTasks);
-                var groupdResults = supplierQueryResponses.GroupBy(x => x.SupplierId).ToList();
-
-                var supplierOrderTasks = new List<Task>();
-                foreach(var group in groupdResults)
-                {
-                    var supplierOrder = new SupplierOrder
-                    {
-                        SupplierId = group.Key,
-                        OrderedItems = group.Select(x =>
-                        {
-                            return new SupplierOrderIngredientItem
-                            {
-                                Name = x.IngredientName,
-                                Quantity = Constants.DefaultUrgentIngredientOrderQuantity
-                            };
-                        }).ToList()
-                    };
-
-                    supplierOrderTasks.Add(await context.CallActivityAsync(Constants.CreateSupplierOrderActivityFunctionName,));
-                }
-
-                await Task.WhenAll(supplierOrderTasks);
-
-                // check and reserve
+                ingredientsToOrder = await HandleNeededStock(context, leftOverNeededIngredients);
             }
 
             return true;
+        }
+
+        private static async Task CreateSupplierOrders(DurableOrchestrationContext context,
+            List<IGrouping<string, SupplierQueryResponse>> groupdResults)
+        {
+            var supplierOrderTasks = new List<Task>();
+            foreach (var group in groupdResults)
+            {
+                var supplierOrder = new SupplierOrder
+                {
+                    SupplierId = group.Key,
+                    OrderedItems = group.Select(x =>
+                    {
+                        return new SupplierOrderIngredientItem
+                        {
+                            Name = x.IngredientName,
+                            Quantity = Constants.DefaultUrgentIngredientOrderQuantity
+                        };
+                    }).ToList()
+                };
+
+                supplierOrderTasks.Add(
+                    context.CallActivityAsync(Constants.CreateSupplierOrderActivityFunctionName, supplierOrder));
+            }
+
+            await Task.WhenAll(supplierOrderTasks);
+        }
+
+        private static async Task<List<IGrouping<string, SupplierQueryResponse>>> GetNeededSuppliers(
+            DurableOrchestrationContext context, List<string> ingredientsToOrder)
+        {
+            var supplierQueryTasks = new List<Task<SupplierQueryResponse>>();
+
+            foreach (var ingredient in ingredientsToOrder)
+            {
+                supplierQueryTasks.Add(context.CallActivityAsync<SupplierQueryResponse>(
+                    Constants.SupplierFinderActivityFunctionName,
+                    new SupplierQueryRequest
+                    {
+                        IngredientName = ingredient,
+                        QueryStrategy = SupplierQueryStrategy.OptimizeDelivery
+                    }));
+            }
+
+            var supplierQueryResponses = await Task.WhenAll(supplierQueryTasks);
+            var groupdResults = supplierQueryResponses.GroupBy(x => x.SupplierId).ToList();
+            return groupdResults;
+        }
+
+        private static async Task<List<string>> HandleNeededStock(DurableOrchestrationContext context,
+            List<DishIngredient> neededIngredients)
+        {
+            var checkTasks = new List<Task<(bool Reserved, string IngredientName)>>();
+
+            foreach (var ingredient in neededIngredients)
+            {
+                checkTasks
+                    .Add(context.CallActivityAsync<(bool Reserved, string IngredientName)>(
+                        Constants.CheckAndReserveStockActivityFunctionName, ingredient));
+            }
+
+            var checkResult = await Task.WhenAll(checkTasks);
+            var ingredientsToOrder = checkResult
+                .Where(x => x.Reserved == false && !string.IsNullOrEmpty(x.IngredientName))
+                .Select(x => x.IngredientName).ToList();
+            return ingredientsToOrder;
         }
 
         [FunctionName("StockCheckerOrchestrator_HttpStart")]
