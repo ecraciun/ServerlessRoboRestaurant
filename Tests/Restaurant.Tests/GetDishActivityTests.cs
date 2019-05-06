e9436b61c5e12d2c1ba30973d10a2206104bd2a3
Emil Craciun <emil.craciun@live.com>
2019-05-06 15:24:56 +0300

    GetDishActivity tests

--- Tests/Restaurant.Tests/GetDishActivityTests.cs
+++ Tests/Restaurant.Tests/GetDishActivityTests.cs
@@ -1,14 +1,43 @@
-using System;
+using Core.Entities;
+using Core.Services.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Threading.Tasks;
+using TestsCommon;
 using Xunit;
 
 namespace Restaurant.Tests
 {
     public class GetDishActivityTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly Mock<IBaseRepositoryFactory<Dish>> _repoFactoryMock;
+        private readonly Mock<IBaseRepository<Dish>> _repoMock;
+        private readonly ILogger _logger = TestFactory.CreateLogger();
 
+        public GetDishActivityTests()
+        {
+            _repoMock = new Mock<IBaseRepository<Dish>>();
+            _repoMock.Setup(x => x.GetAsync(It.IsAny<string>())).Returns(Task.FromResult(new Dish { Id = "test" }));
+
+            _repoFactoryMock = new Mock<IBaseRepositoryFactory<Dish>>();
+            _repoFactoryMock.Setup(x => x.GetInstance(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(_repoMock.Object);
+        }
+
+        [Fact]
+        public async Task Run_Returns_Instance_When_Input_Is_Not_Null()
+        {
+            var result = await GetDishActivity.Run("abc", _repoFactoryMock.Object, _logger);
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Run_Returns_Null_When_Input_Is_Null()
+        {
+            var result = await GetDishActivity.Run(null, _repoFactoryMock.Object, _logger);
+
+            Assert.Null(result);
         }
     }
 }
